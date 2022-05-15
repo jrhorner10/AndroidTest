7b069d63678b0e2e39c70c0044954ddd87e34512
James Horner <jhorner@oakland.edu>
2022-05-15 16:52:37 -0400

    another change to main

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //add comment here
         int x = 5; //comment
-        int y = 4;
+        int y = 4; //comment
     }
 }
