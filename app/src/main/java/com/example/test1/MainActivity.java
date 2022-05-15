fab363773bac1df03da1f754e5a9254d7b7c6fd2
jrhorner10 <88461909+jrhorner10@users.noreply.github.com>
2022-05-15 17:41:51 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //add comment here
-        int x = 5; //comment hey hi
+        int x = 5; //comment hey hi hello
         int y = 4; //comment
     }
 }
